c9abaccba280d21a9696daf6369bfcc0732f4586
DarkPrince3738 <darkprince3738@gmail.com>
2024-01-22 21:19:39 +0100

    added profilePicturePath

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-
 const express = require('express')
+const cookieParser = require("cookie-parser")
 
 const app = express()
 const port = 3001
 
 const {database} = require("./database");
+const {passwordToggle} = require("./public/login-page-scripts/password-toggle.js")
 
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
@@ -27,8 +28,8 @@ app.post('/api/login', (req, res)=>{
         const userToCheck = database.users[i]
 
         if(userToCheck.username === req.body.username && userToCheck.password === req.body.password){
-            //res.send("Authenticated!")
             const sessionToken = `${req.body.username}_${Date.now()}`
+            res.cookie('token', sessionToken, {maxAge:150000, httpOnly: false})
             res.send(sessionToken)
             hasAuthenticatedUser = true
             break;
@@ -39,6 +40,42 @@ app.post('/api/login', (req, res)=>{
     }
 
 })
+
+app.get('/api/:username/city', (req, res) => {
+        const user = database.users.find(user => req.params.username === user.username)
+        if (user) {
+            res.send(user.city)
+        }
+        else{
+            res.sendStatus(404)
+        }
+    }
+)
+
+app.get('/api/:username/profile-picture-path', (req, res) => {
+        const user = database.users.find(user => req.params.username === user.username)
+        if (user) {
+            res.send( user.profilePicturePath)
+        }
+        else{
+            res.sendStatus(404)
+        }
+    }
+)
+/*const user = database.users.find(user => req.params.username === user.username)
+if (user) {
+    res.send(user.city)
+}
+else{
+    res.sendStatus(404)
+}
+)*/
+
+app.get('/dashboard', (req, res)=>{
+    //const isLogged = req.cookies.token
+    res.sendFile(__dirname + "/public/dashboard.html")
+}
+)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const database = {
     users:[
-        {username: 'alex', password: 'pass123'},
-        {username: 'mike', password: "098ssap"},
-        {username: 'lord', password: '456lol'}
+        {username: 'alex', password: 'pass123', city: "Kyiv", profilePicturePath: '/profile-pictures/alex.jpg'},
+        {username: 'mike', password: "456ssap", city: "Lviv", profilePicturePath: '/profile-pictures/mike.jpg'},
+        {username: 'lord', password: '789lol', city: "Berlin", profilePicturePath: '/profile-pictures/lord.jpg'}
     ]
 }
 
