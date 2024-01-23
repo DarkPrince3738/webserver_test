edc6108095d40885f0b61dfa5518435fbb6acd10
DarkPrince3738 <darkprince3738@gmail.com>
2024-01-23 10:46:26 +0100

    reworked username /dashboard

---
+++ public/get-username.js
@@ -0,0 +1,9 @@
+function getUsername(){
+    const name = document.getElementById("get-name")
+    name.textContent = document.cookie
+    const firstPart = name.textContent.indexOf('=')
+    const lastPart  = name.textContent.indexOf('_')
+    name.textContent = document.cookie.slice(firstPart+1, lastPart)
+    console.log(document.cookie)
+}
+window.onload =  ()=> getUsername()
